bfdafcea289a7e2ae8dd6cb59ee6995e8c7dd6ef
koenmoret <koen@moret.info>
2023-12-04 21:39:22 +0100

    feature/huiswerk conditionals

--- conditionals.js
+++ conditionals.js
@@ -8,24 +8,30 @@ console.log('Is 2 gelijk aan 2?', equalNumbers);
 // ==========================================
 // 1. Declareer een variabele en sla daar de uitkomst van de vergelijking "Is 10 groter dan 20?" in op        // geeft false wanneer gelogd
 // ==========================================
-
+const biggerThan = 10>20;
+console.log('Is 10 groter dan 20?', biggerThan);
 
 // ==========================================
 // 2. Declareer een variabele en sla daar de uitkomst van de vergelijking "Is 10 ongelijk aan 11?" in op        // geeft true wanneer gelogd
 // ==========================================
-
+const notTheSame = 10!==11;
+console.log('10 ongelijk aan 11?', notTheSame);
 
 // ==========================================
 // 3. Declareer een variabele en sla daar in op of de woorden 'zoet' en 'sappig' hetzelfde zijn                // geeft false wanneer gelogd
 // ==========================================
-
+const theSameWords = "zoet" === "sappig";
+console.log("Zijn de woorden 'zoet' en 'sappig' hetzelfde?",theSameWords);
 
 // ==========================================
 // 4a. Declareer een variabele en sla daar het rapportcijfer 9 in op.
 // 4b. schrijf een script dat checkt of het rapportcijfer groter is dan 8.
 //     Als dat zo is, loggen we 'Cum laude!' in de console.                                                     // geeft 'Cum laude!' wanneer gelogd
 // ==========================================
-
+const grade = 9;
+if(grade > 8){
+   console.log("Cum laude!");
+}
 
 // ==========================================
 // 5a. Gebruik onderstaande variabelen isRaining en temperature;
@@ -36,3 +42,10 @@ console.log('Is 2 gelijk aan 2?', equalNumbers);
 
 const isRaining = false;
 const temperature = 23;
+if(!isRaining && temperature > 21){
+    console.log("Je kunt een korte broek aan!");
+}else if(isRaining || temperature < 12){
+    console.log("Doe een jas aan!");
+}else {
+    console.log("Lange kleren is prima!");
+}
